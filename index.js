122304f1b643c98c1624b6297c5a80c650bb01cd
george.robakidze-at-334057996933 <robakidzegeorge@gmail.com>
2019-01-31 14:32:13 +0200

    Paginationless demo

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 'use strict'
 const AWS = require('aws-sdk')
-const tools = require('./modules/tools.js')
+const tools = require('./custom-modules/tools.js')
 
 const Tools = tools()
 const DynamoDB = new AWS.DynamoDB.DocumentClient()
 
 module.exports.handler = async function (event, context, callback) {
   try {
-    let data = []
+    let data = {dateTime: (new Date()).toISOString()}
     let { members } = await Tools.httpsGetRequest('https://slack.com/api/users.list?token=' + process.env.APPLICATION_TOKEN + '&presence=true')
 
     members.forEach(function (member) {
       if (member.is_bot === false && member.deleted === false && member.name !== 'slackbot') {
-        data.push({
-          slackName: member.name,
-          presenceStr: member.presence,
-          presenceInt: member.presence === 'active' ? 1 : 0,
-          dateTime: Tools.getDateTime()
-        })
+        data[member.name] = {
+           presenceStr: member.presence,
+           presenceInt: member.presence == 'active' ? 1 : 0
+        }
       }
     })
-
-    data.forEach(function (item) {
-      let params = {
-        TableName: process.env.PRESENCE_TABLE,
-        Item: item
-      }
-      DynamoDB.put(params, function (err, data) {
-        if (err !== null) console.log(err)
-      })
+    console.log(data)
+    let params = {
+      TableName: process.env.PRESENCE_TABLE,
+      Item: data
+    }
+    DynamoDB.put(params, function (err, data) {
+      if (err !== null) console.log(err)
+      console.log("Err: " + JSON.stringify(err))
+      console.log("Data: " + JSON.stringify(data))
     })
   } catch (err) {
     console.log(err)
