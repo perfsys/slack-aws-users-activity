a5578ddef68eb3964b069c48124f820151d11466
george.robakidze-at-334057996933 <robakidzegeorge@gmail.com>
2019-02-19 11:09:25 +0200

    Slack-checker 1.0

--- custom-modules/tools.js
+++ custom-modules/tools.js
@@ -10,13 +10,6 @@ const Tools = function () {
       })
     })
   }
-  this.getDateTime = function (now = null) {
-    if (now == null) now = new Date()
-
-    let date = [now.getFullYear(), ((now.getMonth() + 1) < 10 ? '0' : '') + (now.getMonth() + 1), (now.getDate() < 10 ? '0' : '') + now.getDate()]
-    let time = [(now.getHours() < 10 ? '0' : '') + now.getHours(), (now.getMinutes() < 10 ? '0' : '') + now.getMinutes(), (now.getSeconds() < 10 ? '0' : '') + now.getSeconds()]
-    return date.join('-') + ' ' + time.join(':')
-  }
 }
 
 module.exports = function () { return new Tools() }
--- index.js
+++ index.js
@@ -4,31 +4,46 @@ const tools = require('./custom-modules/tools.js')
 
 const Tools = tools()
 const DynamoDB = new AWS.DynamoDB.DocumentClient()
+const getData = async function(url, params = null) {
+  if(params === null) params = {requestTimeout: 0, timeoutStep: 500, timeoutLimit: 3000}
+  return new Promise(async function(resolve, reject) {
+    try {
+      let dataToDB = {}
+      let { members, response_metadata } = await Tools.httpsGetRequest(url)
+      members.forEach(function (member) {
+        if (member.is_bot === false && member.deleted === false && member.name !== 'slackbot') {
+          dataToDB[member.name] = {
+             presenceStr: member.presence,
+             presenceInt: member.presence === 'active' ? 1 : 0
+          }
+        }
+      })
+      if(typeof(response_metadata.next_cursor) === 'string' && response_metadata.next_cursor.length > 0) setTimeout(async function() {
+        Object.assign(dataToDB, await getData(response_metadata.next_cursor, params))
+      }, params.requestTimeout>=params.timeoutLimit?params.requestTimeout:params.requestTimeout+=params.timeoutStep)
+      resolve(dataToDB)
+    }
+    catch(err) {
+      reject(err)
+    }
+  })
+}
+const dynamoPut = function(params) {
+  return new Promise(function(resolve, rejects) {
+    DynamoDB.put(params, function(err, data) {
+      if(err !== null) reject(err)
+      else resolve(data)
+    })
+  })
+}
 
 module.exports.handler = async function (event, context, callback) {
   try {
-    let data = {dateTime: (new Date()).toISOString()}
-    let { members } = await Tools.httpsGetRequest('https://slack.com/api/users.list?token=' + process.env.APPLICATION_TOKEN + '&presence=true')
-
-    members.forEach(function (member) {
-      if (member.is_bot === false && member.deleted === false && member.name !== 'slackbot') {
-        data[member.name] = {
-           presenceStr: member.presence,
-           presenceInt: member.presence == 'active' ? 1 : 0
-        }
-      }
-    })
-    console.log(data)
-    let params = {
-      TableName: process.env.PRESENCE_TABLE,
-      Item: data
-    }
-    DynamoDB.put(params, function (err, data) {
-      if (err !== null) console.log(err)
-      console.log("Err: " + JSON.stringify(err))
-      console.log("Data: " + JSON.stringify(data))
-    })
+    let dataToDB = await getData('https://slack.com/api/users.list?token=' + process.env.APPLICATION_TOKEN + '&presence=true')
+    dataToDB.dateTime = (new Date()).toISOString()
+    let response = await dynamoPut({ TableName: process.env.PRESENCE_TABLE, Item: dataToDB })
+    console.log('Presence cut was successfully recorded.\n' + JSON.stringify(response))
   } catch (err) {
-    console.log(err)
+    console.log('An error occured while recording presence cut:\n' + JSON.stringify(err))
   }
 }
